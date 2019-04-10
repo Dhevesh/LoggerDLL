6650df4147890e9525be66b5b2f0edc74a84e60e
IMS\Dheveshp <dheveshp@gmail.com>
2019-04-10 17:18:46 +0100

    Removed local variables to fields.

--- LoggerDLL/LoggerDLL/Logger.cs
+++ LoggerDLL/LoggerDLL/Logger.cs
@@ -5,7 +5,11 @@ namespace LoggerDLL
 {
     public class Logger
     {
-        private readonly string filePath = @"c:\TestLogs\";
+        private readonly static string filePath = @"c:\TestLogs\"; // TODO this will take in the value from the configuration file
+        private readonly static string date = $"{DateTime.Now:yyyyMMdd}";
+        private readonly string time = $"{DateTime.Now.ToShortTimeString()} [ERROR]";
+        private readonly string file = $"{filePath}{date} Log.txt"; 
+        
         internal void CheckDirectory()
         {
             if (!Directory.Exists(filePath))
@@ -17,10 +21,8 @@ namespace LoggerDLL
         public void ErrorLog(string e) //Takes in the Exception and logs to file
         {
             CheckDirectory();
-            string date = $"{DateTime.Now:yyyyMMdd}";
-            string time = $"{DateTime.Now.ToShortTimeString()} [ERROR]";
-            string file = $"{filePath}{date} Log.txt";
-            if (File.Exists(file))
+            
+            if (File.Exists(file)) // TODO set the file size. Roll over to a new file if the limit is reached.
             {
                 using (var fileWriter = new StreamWriter(file, true))
                 {
