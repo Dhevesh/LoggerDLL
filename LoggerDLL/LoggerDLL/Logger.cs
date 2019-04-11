1728abddc7c4914abac186f49c29c34b973afd79
IMS\Dheveshp <dheveshp@gmail.com>
2019-04-11 11:50:25 +0100

    separated writing to file into it's own method to be called in different scenarios.

--- LoggerDLL/LoggerDLL/Logger.cs
+++ LoggerDLL/LoggerDLL/Logger.cs
@@ -7,7 +7,7 @@ namespace LoggerDLL
     {
         private static string filePath = @"c:\LoggerDLL\"; //This is used as a default if the user does not set their own path in their application.
         private readonly static string date = $"{DateTime.Now:yyyyMMdd}";
-        private readonly string time = $"{DateTime.Now.ToShortTimeString()} [ERROR]";
+        private readonly string time = $"{DateTime.Now.ToShortTimeString()}";
         private static string file = $"{filePath}{date} Log.txt"; //This is used as a default if the user does not set their own name in their application.
 
         /// <summary>
@@ -29,7 +29,12 @@ namespace LoggerDLL
             }
         }
 
-        internal void WriteToFile(string e)
+        /// <summary>
+        /// Method to write the information passed to it to a text file.
+        /// </summary>
+        /// <param name="outPutText">string value. the text to be written to the log file</param>
+        /// <param name="type">string value. type of text (error, info, warning)</param>
+        internal void WriteToFile(string outPutText, string type)
         {
             CheckDirectory();
 
@@ -37,32 +42,33 @@ namespace LoggerDLL
             {
                 using (var fileWriter = new StreamWriter(file, true))
                 {
-                    fileWriter.WriteLine($"{time} {e}");
+                    fileWriter.WriteLine($"{time} {type} {outPutText}");
                 }
             }
             else
             {
                 using (var fileWriter = new StreamWriter(file, true))
                 {
-                    fileWriter.WriteLine($"{time} {e}");
+                    fileWriter.WriteLine($"{time} {type} {outPutText}");
                 }
             }
 
         }
 
-        public void Error(string e)
+
+        public void Error(string error)
         {
-            //TODO 
+            WriteToFile(error, "[ERROR]");
         }
 
-        public void Info(string e)
+        public void Info(string info)
         {
-            //TODO
+            WriteToFile(info, "[INFO]");
         }
 
-        public void Warning(string e)
+        public void Warning(string warning)
         {
-            //TODO
+            WriteToFile(warning, "[WARNING]");
         }
     }
 }
